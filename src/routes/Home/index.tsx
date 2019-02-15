730480900877da0f4bf58dc29458aa682911b07b
ViZhe <vizhe.cat@ya.ru>
2019-02-15 17:39:09 +0300

    feat: Add base store & Home route

---
+++ src/routes/Home/index.tsx
@@ -0,0 +1,21 @@
+
+import React, { Component } from 'react';
+
+import './index.css';
+
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="home">
+        <div className="home__box">
+          <div className="home__header">Hello, anonym!</div>
+          <div className="home__body">wow wow</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+
+export default Home;
