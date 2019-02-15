211f89cd2ccc2a2479a4c24814af86756551b7b7
ViZhe <vizhe.cat@ya.ru>
2019-02-15 19:38:16 +0300

    refactor: Change class to function

--- src/routes/NotFound/index.tsx
+++ src/routes/NotFound/index.tsx
@@ -1,25 +1,21 @@
 
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 import './index.css';
 
 
-class Second extends Component {
-  render() {
-    return (
-      <div className="notFound">
-        <div className="notFound__box">
-          <div className="notFound__header">Oops, hmmm!</div>
-          <div className="notFound__body">Something went wrong.</div>
-          <div className="notFound__footer">
-            <Link to="/" className="notFound__btn">Home</Link>
-          </div>
-        </div>
+const NotFound: React.FC = () => (
+  <div className="notFound">
+    <div className="notFound__box">
+      <div className="notFound__header">Oops, hmmm!</div>
+      <div className="notFound__body">Something went wrong.</div>
+      <div className="notFound__footer">
+        <Link to="/" className="notFound__btn">Home</Link>
       </div>
-    );
-  }
-}
+    </div>
+  </div>
+);
 
 
-export default Second;
+export default NotFound;
