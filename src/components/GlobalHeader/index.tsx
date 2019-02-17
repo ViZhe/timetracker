27c7c1c9691609112474566fc56e4709071d7537
ViZhe <vizhe.cat@ya.ru>
2019-02-17 13:22:11 +0300

    feat: Add component GlobalHeader

---
+++ src/components/GlobalHeader/index.tsx
@@ -0,0 +1,31 @@
+
+import React, { Component } from 'react';
+import { Layout, Icon } from 'antd';
+
+import styles from './index.module.css';
+
+
+type Props = {
+  collapsed: boolean;
+  onClickTrigger: () => void;
+};
+
+const { Header } = Layout;
+
+class GlobalHeader extends Component<Props> {
+  render() {
+    const { collapsed, onClickTrigger } = this.props;
+    return (
+      <Header style={{ background: '#fff', padding: 0 }}>
+        <Icon
+          className={styles.trigger}
+          type={collapsed ? 'menu-unfold' : 'menu-fold'}
+          onClick={onClickTrigger}
+        />
+      </Header>
+    );
+  }
+}
+
+
+export default GlobalHeader;
