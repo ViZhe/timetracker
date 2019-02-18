cece7dc1aac775f87bc975391279f250ac2983f1
ViZhe <vizhe.cat@ya.ru>
2019-02-18 16:20:24 +0300

    feat: Update routes with new route files

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,24 +1,16 @@
 
-import React, { lazy, Suspense } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import React, { lazy } from 'react';
+import {  Route, Switch } from 'react-router-dom';
 
 
 const Home = lazy(() => import('./Home'));
-const MyReportsProjects = lazy(() => import('./MyReportsProjects'));
-const MySettings = lazy(() => import('./MySettings'));
 const My = lazy(() => import('./My'));
 const NotFound = lazy(() => import('./NotFound'));
 
 const routes = (
   <Switch>
     <Route path="/" component={Home} exact={true} />
-
-    <Switch>
-      <Route path="/my/reports/projects"component={MyReportsProjects} />
-      <Route path="/my/settings" component={MySettings} />
-      <Route path="/my" component={My} />
-    </Switch>
-
+    <Route path="/my" component={My} />
     <Route component={NotFound} />
   </Switch>
 );
