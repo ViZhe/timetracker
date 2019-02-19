e318fb352dc3cda21b2bf1d187bec5bb1018f781
ViZhe <vizhe.cat@ya.ru>
2019-02-19 12:25:57 +0300

    feat: Add form on Base setting page

--- src/routes/Settings/Base/index.tsx
+++ src/routes/Settings/Base/index.tsx
@@ -1,12 +1,63 @@
 
-import React from 'react';
+import React, { Component } from 'react';
+import {
+  Form, Input, Tooltip, Icon, Button, message,
+} from 'antd';
+import { FormComponentProps } from 'antd/lib/form';
+
+import styles from './index.module.css';
 
 
-const Base = () => (
-  <div>
-    {'hello base'}
-  </div>
-);
+type Props = {};
+type State = {};
+
+class Base extends Component<Props & FormComponentProps, State> {
+  readonly state: State = {};
+
+  handleSubmit = (e: React.SyntheticEvent<HTMLElement>) => {
+    e.preventDefault();
+    this.props.form.validateFieldsAndScroll((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values);
+        message.success('Success');
+        return;
+      }
+      message.error('Error');
+    });
+  };
+
+  render() {
+    const { getFieldDecorator } = this.props.form;
+    return (
+      <div className={styles.wrapper}>
+        <h1 className={styles.title}>Base Settings</h1>
+        <Form layout="vertical" onSubmit={this.handleSubmit}>
+          <Form.Item
+            label={(
+              <span>
+                Name&nbsp;
+                <Tooltip title="What do you want others to call you?">
+                  <Icon type="question-circle-o" />
+                </Tooltip>
+              </span>
+            )}
+          >
+            {getFieldDecorator('name', {
+              rules: [{ required: true, message: 'Please input your name!', whitespace: true }],
+            })(
+              <Input />
+            )}
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit">Update</Button>
+          </Form.Item>
+        </Form>
+      </div>
+    );
+  }
+}
 
 
-export default React.memo(Base);
+export default Form.create<FormComponentProps>({
+  name: 'register',
+})(Base);
