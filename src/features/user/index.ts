474fad97bdd0ba22878653f760dfcef248342fb0
ViZhe <vizhe.cat@ya.ru>
2019-02-21 15:15:51 +0300

    feat: Add features user index exports

---
+++ src/features/user/index.ts
@@ -0,0 +1,13 @@
+
+import * as userActions from './actions';
+import * as userConstants from './constants';
+import reducer from './reducer';
+import * as userSelectors from './selectors';
+
+
+export {
+  userConstants,
+  userActions,
+  reducer,
+  userSelectors,
+};
