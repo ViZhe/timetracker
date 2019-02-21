7457e75d49b43701aeb1d0a5f2f08d0a879d78ce
ViZhe <vizhe.cat@ya.ru>
2019-02-21 15:16:37 +0300

    feat: Add userReducer in rootReducer

--- src/store/reducers.ts
+++ src/store/reducers.ts
@@ -3,11 +3,10 @@ import { connectRouter, RouterState } from 'connected-react-router';
 import { History } from 'history';
 import { combineReducers } from 'redux';
 
+import { reducer as userReducer } from '../features/user';
 
-export interface IState {
-  router: RouterState;
-}
 
 export const createRootReducer = (history: History) => combineReducers({
   router: connectRouter(history),
+  user: userReducer,
 });
