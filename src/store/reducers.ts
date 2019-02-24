4a0029a99e38723b6a911931517ad18330629e39
ViZhe <vizhe.cat@ya.ru>
2019-02-24 15:41:03 +0300

    feat: Add timesReducer in rootReducer

--- src/store/reducers.ts
+++ src/store/reducers.ts
@@ -3,10 +3,12 @@ import { connectRouter, RouterState } from 'connected-react-router';
 import { History } from 'history';
 import { combineReducers } from 'redux';
 
+import { reducer as timesReducer } from '../features/times';
 import { reducer as userReducer } from '../features/user';
 
 
 export const createRootReducer = (history: History) => combineReducers({
   router: connectRouter(history),
+  times: timesReducer,
   user: userReducer,
 });
