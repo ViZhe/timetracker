65b9122b4861adcb1f748656f04e9e7372392787
ViZhe <vizhe.cat@ya.ru>
2019-02-24 15:33:12 +0300

    feat: Add features times index exports

---
+++ src/features/times/index.ts
@@ -0,0 +1,13 @@
+
+import * as timesActions from './actions';
+import * as timesConstants from './constants';
+import reducer from './reducer';
+import * as timesSelectors from './selectors';
+
+
+export {
+  timesConstants,
+  timesActions,
+  timesSelectors,
+  reducer,
+};
