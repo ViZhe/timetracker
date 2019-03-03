32610abe881ab4ca63f28bc8f5e4c086a4a35775
ViZhe <vizhe.cat@ya.ru>
2019-03-03 15:53:59 +0300

    test: Rewrite with cover line 20

--- src/components/GlobalHeader/__tests__/index.spec.tsx
+++ src/components/GlobalHeader/__tests__/index.spec.tsx
@@ -7,29 +7,38 @@ import GlobalHeader from '../../GlobalHeader';
 
 
 describe('<GlobalHeader />', () => {
-  let component:ShallowWrapper;
-  const collapsed = false;
-  const onClick = jest.fn();
+  const props = {
+    collapsed: false,
+    onClickTrigger: jest.fn(),
+  };
 
-  beforeEach(() => {
-    component = shallow(
-      <GlobalHeader
-        collapsed={collapsed}
-        onClickTrigger={onClick}
-      />,
-    );
+  describe('<GlobalHeader /> initial', () => {
+    const globalHeader:ShallowWrapper = shallow(<GlobalHeader {...props} />);
+
+    it('should match snapshot', () => {
+      expect(globalHeader).toMatchSnapshot();
+    });
+
+    it('should be not collapsed', () => {
+      expect(globalHeader.find(Icon).prop('type')).toEqual('menu-fold');
+    });
+
+    it('should call onClick once', () => {
+      globalHeader.find(Icon).simulate('click');
+      expect(props.onClickTrigger).toHaveBeenCalledTimes(1);
+    });
   });
 
-  it('should match snapshot', () => {
-    expect(component).toMatchSnapshot();
-  });
+  describe('<GlobalHeader /> props collapsed is true', () => {
+    const nextProps = {
+      ...props,
+      collapsed: true,
+    };
 
-  it('should be collapsed by default', () => {
-    expect(component.find(Icon).get(0).props.type).toEqual('menu-fold');
-  });
+    const globalHeader:ShallowWrapper = shallow(<GlobalHeader {...nextProps} />);
 
-  it('should call onClick once', () => {
-    component.find(Icon).simulate('click');
-    expect(onClick).toHaveBeenCalledTimes(1);
+    it('should be collapsed', () => {
+      expect(globalHeader.find(Icon).prop('type')).toEqual('menu-unfold');
+    });
   });
 });
